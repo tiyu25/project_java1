00e597449d5c6fb9871727ddc6024de438b18d8f
tiyu <tiyuuuu9@gmail.com>
2024-07-24 19:31:01 +0900

    Level1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,14 +7,18 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요.");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = sc.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요.");
+        System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
 
-        System.out.println("num1 + num2 =" + num1 + "," +num2);
+        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        // charAt(idx): charAt 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치(index)를 받는다.
+        char operator = sc.next().charAt(0);
+
+        System.out.println(operator);
     }
 }
