ec2c38ffdea98d6a547b03b502ae8687d85b8e1d
tiyu <tiyuuuu9@gmail.com>
2024-07-29 12:47:57 +0900

    Level2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,20 +8,16 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        // 연산 결과를 저장할 배열
-        // int[] resultArr = new int[10];
-        List<Integer> resultArr = new ArrayList<>();
+        // Calculator 인스턴스 생성
+        Calculator calculator = new Calculator();
 
-        // 저장된 결과의 개수를 세는 변수
-        //int count = 0;
+        ArrayList<Integer> resultArr = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
-        //결과
         int result = 0;
 
         while(true) {
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
 
@@ -29,34 +25,16 @@ public class App {
             int num2 = sc.nextInt();
 
             System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
-            // charAt(idx): charAt 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치(index)를 받는다.
             char operator = sc.next().charAt(0);
 
-            switch(operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        // num2이 0이 아닐 때
-                        result = num1 / num2;
-                    } else {
-                        // num2이 0일 때
-                        System.out.println("나눗셈 연산에서 두번째 정수에 0이 입력될 수 없습니다.");
-                        continue;
-                    }
-                    break;
-                default:
-                    // 연산자를 잘못 입력했을 때
-                    System.out.println("잘못된 연산자 입니다.");
-                    continue;
+            // 연산 결과 출력 및 저장
+            try {
+                result = calculator.calculate(num1, num2, operator);
+            } catch (Exception e) {
+                // 나눗셈 두번째 정수 0 입력 시 예외처리
+                System.out.println(e.getMessage());
             }
+
             // 결과 출력
             System.out.println("결과: " + result);
 
@@ -90,12 +68,6 @@ public class App {
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }
-
         }
-
-
-
-
-
     }
 }
